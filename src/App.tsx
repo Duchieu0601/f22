5cfc607171dcf28fe4a7094d0bd3fb0d248e5fba
ngocdang2005 <phimma2002a@gmail.com>
2025-04-11 23:48:31 +0700

    login

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useRoutes } from "react-router";
+import ProductList from "./pages/product/list";
+import ProductAdd from "./pages/product/add";
+import ProductEdit from "./pages/product/edit";
+import { Layout, Menu } from "antd";
+import "antd/dist/reset.css"; // Ant Design v5 reset styles
+import { Link } from "react-router-dom";
+import Register from "./pages/auth/register";
+import UserList from "./pages/user/list";
+import Login from "./pages/auth/login";
+import AdminLayout from "./layout/Admin";
+import ClientLayout from "./layout/Client";
+
+const { Header } = Layout;
+
+function App() {
+  const element = useRoutes([
+    {
+      path: "/admin",
+      element: <AdminLayout />,
+      children: [
+        {
+          path: "products",
+          element: <ProductList />,
+        },
+
+        {
+          path: "product/add",
+          element: <ProductAdd />,
+        },
+        {
+          path: "product/:id/edit",
+          element: <ProductEdit />,
+        },
+        {
+          path: "users",
+          element: <UserList />,
+        },
+      ],
+    },
+    {
+      path: "",
+      element: <ClientLayout />,
+      children: [
+        {
+          path: "login",
+          element: <Login />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+      ],
+    },
+  ]);
+  return <main>{element}</main>;
+}
+
+export default App;
